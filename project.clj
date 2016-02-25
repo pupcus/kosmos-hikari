e5d7998ceab212a8e0a110422a48cf8ecad3e560
Michael Pendergrass <mdpendergrass@gmail.com>
2016-02-25 11:38:29 -0500

    prepare for release
     - use clojure.test for testing for consistency with other kosmos components
     - use kosmos-hsqldb-server for testing

--- project.clj
+++ project.clj
@@ -9,16 +9,22 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[kosmos "0.0.2-SNAPSHOT"]
+  :dependencies [[kosmos "0.0.3"]
                  [org.clojure/tools.logging "0.3.1"]
                  [org.clojure/java.jdbc "0.4.2"]
                  [com.zaxxer/HikariCP "2.4.3"]]
 
   :profiles {:dev {:resource-paths ["dev-resources"]
                    :dependencies   [[org.clojure/clojure "1.8.0"]
-                                    [midje "1.9.0-SNAPSHOT"]
-                                    [org.hsqldb/hsqldb "2.3.3"]
-                                    [org.slf4j/slf4j-log4j12 "1.7.5"]]}
-             ;; You can add dependencies that apply to `lein midje` below.
-             ;; An example would be changing the logging destination for test runs.
-             :midje {}})
+                                    [kosmos/kosmos-hsqldb-server "0.0.1"]
+                                    [org.slf4j/slf4j-log4j12 "1.7.5"]]}}
+
+  :deploy-repositories [["snapshots"
+                         {:url "https://clojars.org/repo"
+                          :creds :gpg}]
+                        ["releases"
+                         {:url "https://clojars.org/repo"
+                          :creds :gpg}]]
+
+  :global-vars {*warn-on-reflection* true
+                *assert* false})
