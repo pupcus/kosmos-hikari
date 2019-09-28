4659eca9694463ca7429f3dadb0d405ed5104325
Michael Pendergrass <mdpendergrass@gmail.com>
2019-09-28 09:19:34 -0400

    prepare for release

--- project.clj
+++ project.clj
@@ -9,24 +9,18 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[kosmos "0.0.7"]
-                 [org.clojure/tools.logging "0.4.0"]
-                 [org.clojure/java.jdbc "0.7.3"]
-                 [com.zaxxer/HikariCP "2.7.2"]]
+  :dependencies [[kosmos "0.0.10"]
+                 [org.clojure/tools.logging "0.5.0"]
+                 [org.clojure/java.jdbc "0.7.10"]
+                 [com.zaxxer/HikariCP "3.4.1"]]
 
   :profiles {:dev {:resource-paths ["dev-resources"]
-                   :dependencies   [[org.clojure/clojure "1.8.0"]
-                                    [kosmos/kosmos-hsqldb-server "0.0.3"]
+                   :dependencies   [[org.clojure/clojure "1.10.1"]
+                                    [kosmos/kosmos-hsqldb-server "0.0.4"]
                                     [org.slf4j/slf4j-log4j12 "1.7.25"]]}}
 
-  :deploy-repositories [["snapshots"
-                         {:url "https://clojars.org/repo"
-                          :sign-releases false
-                          :creds :gpg}]
-                        ["releases"
-                         {:url "https://clojars.org/repo"
-                          :sign-releases false
-                          :creds :gpg}]]
+  :deploy-repositories {"releases" {:url "https://repo.clojars.org" :creds :gpg :sign-releases false}
+                        "snapshots" {:url "https://repo.clojars.org" :creds :gpg :sign-releases false}}
 
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
